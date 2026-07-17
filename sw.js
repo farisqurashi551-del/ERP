e4a5cfac7c87c3064921936a36450c980a26433a
farisqurashi551-del <faris.qurashi551@gmail.com>
2026-07-17 10:34:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "erp-v1";
+const CACHE = "erp-v5";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
